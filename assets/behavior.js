047c317eb67085a1cb8a01312d68b5e4a4c08da1
mmullens <mmullens@hoonto.com>
2013-06-30 21:48:21 +0000

    Generated documentation for ffe2583a04105c5648de7930a3bf5e2963a094b5

--- assets/behavior.js
+++ assets/behavior.js
@@ -25,14 +25,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 1,
-            "title": "Node API",
-            "slug": "node-api"
-          },
-          "depth": 1
-        }, {
-          "type": "heading",
-          "data": {
-            "level": 1,
             "title": "Profile",
             "slug": "profile"
           },
